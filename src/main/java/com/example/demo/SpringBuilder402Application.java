3bf72c599443bb5463db9fedbfb076734f4b8cb5
GBTC405002UR <GBTC405002UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-21 11:51:37 -0400

    Project 4.02 - Adding a Custom Login Page

---
+++ src/main/java/com/example/demo/SpringBuilder402Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBuilder402Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBuilder402Application.class, args);
+    }
+
+}
